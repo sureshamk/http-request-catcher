b4a52a6946b8cd9bdabad81994d324b0c322bcf2
suresh <suresh@tricog.com>
2022-01-11 10:32:10 +0530

    sbz

--- pages/api.js
+++ pages/api.js
@@ -1,11 +1,8 @@
 import {join, dirname} from 'path'
 import {fileURLToPath} from 'url'
 const loki = require('lokijs');
-const formidable = require('formidable');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Use JSON file for storage
-const file = join(__dirname, 'example.db')
 var db = new loki('./example.db');
 var requests = db.addCollection('requests');
 export default async function handler(req, res) {
